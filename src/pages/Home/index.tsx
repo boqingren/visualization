59467bbeaa54bcf823107110498deb94b1592a9a
Jun <liangqijun@zgrj.net>
2020-04-24 15:19:02 +0800

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState, useCallback, useMemo } from "react";
+import "./index.css";
 
 const Home = React.memo(() => {
+  const [ count, setCount ] = useState(0);
+
+  const handleClick = useCallback(() => {
+    const result = count + 1;
+    setCount(result);
+  }, [ count ]);
+
+  const countWidget = useMemo(() => (
+    <section className="count-widget-container">
+      <span className="count-widget-text badge badge-secondary">
+        {count}
+      </span>
+      <button className="count-widget-btn btn btn-sm btn-info" onClick={handleClick}>
+        increase
+      </button>
+    </section>
+  ), [ count, handleClick ]);
+
   return (
-    <div>
-      Home Page.
-    </div>
+    <section className="home-page-container">
+      <h1 className="home-page-title">
+        Home Page.
+      </h1>
+      {countWidget}
+    </section>
   );
 });
 
