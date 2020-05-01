f620bfa25de0dad566d0afb7efa146407e9d966c
boqingren <boqingren@hotmail.com>
2020-05-01 15:31:33 +0800

    update Table components

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -2,49 +2,61 @@ import React from "react";
 import { ITableProps } from "../../types";
 import "./index.css";
 
-const Table: React.FC<ITableProps> = React.memo(props => {
+const Pagination = React.memo(props => {
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="pagination table-pagination-container">
+        <li className="page-item">
+          <a className="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li className="page-item"><a className="page-link" href="#">1</a></li>
+        <li className="page-item"><a className="page-link" href="#">2</a></li>
+        <li className="page-item"><a className="page-link" href="#">3</a></li>
+        <li className="page-item">
+          <a className="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+});
+
+const TableList: React.FC<ITableProps> = React.memo(props => {
   const { columns, dataSource } = props;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          {columns.map((item, index) => {
+            const key = item.key || item.dataIndex || index;
+            return <th key={key} scope="col">{item.title}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {dataSource.map((item, index) => (
+          <tr key={index}>
+            {Object.keys(item).map((child, idx) => {
+              const key = columns[idx].dataIndex;
+              const renderColumn = columns[idx].render;
+              return <td key={key}>{renderColumn? renderColumn(item[key], item, index): item[key]}</td>;
+            })}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+);
+});
+
+const Table: React.FC<ITableProps> = React.memo(props => {
   console.log("render table component...");
   return (
     <section className="table-component-container">
-      <table className="table">
-        <thead>
-          <tr>
-            {columns.map((item, index) => {
-              const key = item.key || item.dataIndex || index;
-              return <th key={key} scope="col">{item.title}</th>;
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {dataSource.map((item, index) => (
-            <tr key={index}>
-              {Object.keys(item).map((child, idx) => {
-                const key = columns[idx].dataIndex;
-                const renderColumn = columns[idx].render;
-                return <td key={key}>{renderColumn? renderColumn(item[key], item, index): item[key]}</td>;
-              })}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <nav aria-label="Page navigation example">
-        <ul className="pagination table-pagination-container">
-          <li className="page-item">
-            <a className="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          <li className="page-item"><a className="page-link" href="#">1</a></li>
-          <li className="page-item"><a className="page-link" href="#">2</a></li>
-          <li className="page-item"><a className="page-link" href="#">3</a></li>
-          <li className="page-item">
-            <a className="page-link" href="#" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <TableList {...props} />
+      <Pagination />
     </section>
   );
 });
