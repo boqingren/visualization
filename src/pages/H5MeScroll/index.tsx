6bf655539840ad7be0b31dc3ae41863cd5036e40
boqingren <boqingren@hotmail.com>
2020-06-23 10:32:51 +0800

    update

--- src/pages/H5MeScroll/index.tsx
+++ src/pages/H5MeScroll/index.tsx
@@ -2,7 +2,24 @@ import React from "react";
 import { H5Scroll } from "../../components";
 import { getPageData } from "../../services/H5MeScroll";
 import { useScroll } from "../../hooks";
+import { IUseScrollResult } from "../../types";
 import "./index.css";
+
+const PageListItem = React.memo(props => {
+  return (
+    <div className="h5-mescroll-page-list-item-container">
+      131
+    </div>
+  );
+});
+
+const PageList: React.FC<{ store: IUseScrollResult }> = React.memo(props => {
+  return (
+    <div className="h5-mescroll-page-list-container">
+      
+    </div>
+  );
+});
 
 const H5MeScroll = React.memo(() => {
   console.log("render h5 mescroll page...");
@@ -11,13 +28,7 @@ const H5MeScroll = React.memo(() => {
     <div className="h5-mescroll-page-container">
       <H5Scroll
         getPageData={getPageData}
-        render={store => {
-          return (
-            <div>
-
-            </div>
-          )
-        }}
+        render={store => <PageList store={store} />}
       />
     </div>
   );
